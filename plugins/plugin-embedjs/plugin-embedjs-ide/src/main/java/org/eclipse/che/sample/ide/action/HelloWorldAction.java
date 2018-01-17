64905d4cfaad2999d5dfba9fc9276242cf0d723f
Eugene Ivantsov <eivantsov@redhat.com>
2018-01-17 16:19:28 +0200

    init

---
+++ plugins/plugin-embedjs/plugin-embedjs-ide/src/main/java/org/eclipse/che/sample/ide/action/HelloWorldAction.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2012-2017
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.che.sample.ide.action;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.eclipse.che.ide.api.action.ActionEvent;
+import org.eclipse.che.ide.api.action.BaseAction;
+import org.eclipse.che.ide.api.parts.PartStackType;
+import org.eclipse.che.ide.api.parts.WorkspaceAgent;
+import org.eclipse.che.sample.ide.view.HelloWorldViewPresenter;
+
+/** Action for opening a part which embeds javascript code. */
+@Singleton
+public class HelloWorldAction extends BaseAction {
+
+  public static final String ACTION_ID = "helloWorldFromJSAction";
+
+  private WorkspaceAgent workspaceAgent;
+  private HelloWorldViewPresenter helloWorldViewPresenter;
+
+  @Inject
+  public HelloWorldAction(
+      WorkspaceAgent workspaceAgent, HelloWorldViewPresenter helloWorldViewPresenter) {
+    super("Show Hello World JavaScript View");
+    this.workspaceAgent = workspaceAgent;
+    this.helloWorldViewPresenter = helloWorldViewPresenter;
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    workspaceAgent.openPart(helloWorldViewPresenter, PartStackType.INFORMATION);
+    workspaceAgent.setActivePart(helloWorldViewPresenter);
+  }
+}
---
+++ plugins/plugin-embedjs/plugin-embedjs-ide/src/main/java/org/eclipse/che/sample/ide/view/HelloWorldViewImpl.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2012-2017
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.che.sample.ide.view;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import org.eclipse.che.ide.api.parts.base.BaseView;
+import org.eclipse.che.sample.ide.view.client.jso.HelloWorldViewOverlay;
+
+/** @author Mathias Schaefer <mathias.schaefer@eclipsesource.com> */
+public class HelloWorldViewImpl extends BaseView<HelloWorldView.ActionDelegate>
+    implements HelloWorldView {
+
+  interface HelloWorldViewImplUiBinder extends UiBinder<Widget, HelloWorldViewImpl> {}
+
+  private static final HelloWorldViewImplUiBinder UI_BINDER =
+      GWT.create(HelloWorldViewImplUiBinder.class);
+
+  @UiField FlowPanel helloWorldPanel;
+
+  @Inject
+  public HelloWorldViewImpl() {
+    setContentWidget(UI_BINDER.createAndBindUi(this));
+  }
+
+  @Override
+  public void sayHello(String content) {
+    HelloWorldViewOverlay.sayHello(helloWorldPanel.getElement(), content);
+    helloWorldPanel.setVisible(true);
+  }
+}
